1055f3786129483a519fcbdac1f4732cd395e9d8
sharyu raut <sharyukraut210298@gmail.com>
2020-09-26 22:02:26 +0530

    [Sharyu Raut] Add.commit first

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage computation Program!");
+        }
+    }
+}
