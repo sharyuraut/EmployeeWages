5e5c6e57eb1eea97bb864ac397fd12dceb01722b
sharyu raut <sharyukraut210298@gmail.com>
2020-09-26 23:32:56 +0530

    [Sharyu] Add.uc3 part time wages

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -9,9 +9,11 @@ namespace EmployeeWages
             Console.WriteLine("Welcome to Employee Wage computation Program!");
 
             //Constants
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
             int FULL_TIME = 8;
+            int PART_TIME = 4;
 
             Random random = new Random();
 
@@ -22,6 +24,11 @@ namespace EmployeeWages
             {
                 Console.WriteLine("Employee is Present");
                 Console.WriteLine("Employee daily Wages = " + EMP_RATE_PER_HOUR*FULL_TIME);
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is part time available");
+                Console.WriteLine("Employee daily Wages = " + EMP_RATE_PER_HOUR*PART_TIME);
             }
             else
             {
