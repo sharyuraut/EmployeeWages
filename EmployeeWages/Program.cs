bdd33347eb74eaa73f65d1eda8af121d23354432
sharyu raut <sharyukraut210298@gmail.com>
2020-09-26 22:29:36 +0530

    [Sharyu raut] Add.usecase1 code to uc1

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,6 +7,22 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage computation Program!");
+
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            //Computations
+            int empCheck = random.Next(0, 2);
+
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
