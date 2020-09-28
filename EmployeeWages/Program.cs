96c288a2248cd3e596627cf499dbbd1b62ea9831
sharyu raut <sharyukraut210298@gmail.com>
2020-09-28 09:57:06 +0530

    [Sharyu] Add.uc8

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace EmployeeWages
 {
@@ -7,21 +8,28 @@ namespace EmployeeWages
         //Constants
         public const int IS_FULL_TIME = 2;
         public const int IS_PART_TIME = 1;
-        public const int EMP_RATE_PER_HOUR = 20;
+        //public const int EMP_RATE_PER_HOUR = 20;
         public const int FULL_TIME = 8;
         public const int PART_TIME = 4;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
+        //public const int NUM_OF_WORKING_DAYS = 20;
+        //public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage computation Program!");
 
+            //calling computeWage method with required parameters
+            computeWage("ABC", 20, 2, 100);
+            computeWage("XYZ", 18, 3, 80);
+        }
+
+        public static void computeWage(String Company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+        {
             //Variables
             int empHrs = 0;
             int empWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
             //Computation
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -48,7 +56,10 @@ namespace EmployeeWages
             }
 
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            //displaying company name, total employee wages and total employee hours
+            Console.WriteLine("Company: " + Company);
             Console.WriteLine("Total Employee Wage = " + totalEmpWage);
+            Console.WriteLine("Total Employee Hours are: " + totalEmpHrs);
         }
     }
 }
